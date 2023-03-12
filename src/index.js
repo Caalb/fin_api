a9f428227d6e0b59a206c860135ec29c540e4307
Caalb <carlosmunizfjunior@gmail.com>
2023-03-12 19:37:10 -0300

    feat(index): adding a temporary "database"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(express.json());
 
+const customers = [];
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
-  response.json({ message: "Test" })
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send(customers)
 })
 
 app.listen(3333)
