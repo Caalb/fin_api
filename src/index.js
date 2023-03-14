6feb4a84cd344cd881f3ec23177f8df4468935d4
Caalb <carlosmunizfjunior@gmail.com>
2023-03-13 22:07:33 -0300

    feat(index): create method to change customer name && method to get data customer account

--- src/index.js
+++ src/index.js
@@ -109,4 +109,20 @@ app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(statement);
 })
 
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send(customer);
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
+
 app.listen(3333)
